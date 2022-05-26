340edbc6022246488214ca57d585d8fda6a78041
Panfile Kirill <95650362+kirillpanfile@users.noreply.github.com>
2022-05-26 14:07:36 +0300

    created bot

--- index.js
+++
@@ -1,7 +0,0 @@
-var win = [1, 2, 3];
-let lose = [4, 5, 6];
-for (var i in win) {
-  for (let j in lose) {
-    console.log(i);
-  }
-}
--- main.js
+++ main.js
@@ -1,6 +1,26 @@
-import './style.css'
+const { Telegraf } = require("telegraf");
+const bot = new Telegraf("5308585791:AAEkHC1bUWui4Fbpsbc7irvTpbK6Grinm_8");
+const express = require("express");
+const cors = require("cors");
+const ytdl = require("ytdl-core");
+const app = express();
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+bot.command("start", (ctx) => {
+  console.log(ctx.from);
+  bot.telegram.sendMessage(
+    ctx.chat.id,
+    "hello there! Welcome to my new telegram bot.",
+    {}
+  );
+});
+
+function sendUrl(url) {
+  console.log(url);
+}
+
+//bot hears everyfing
+bot.on("text", (ctx) => {
+  sendUrl(ctx.message.text);
+});
+
+bot.launch();
